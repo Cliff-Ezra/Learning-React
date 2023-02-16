aaa7d861dfa9a7e48e8195b0f9c80a5ec5cdff0c
Cliff-Ezra <cliffezra212@gmail.com>
2023-02-16 20:39:53 +0300

    Adding Event Listeners

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,17 @@
 import classes from './NewPost.module.css';
 
 function NewPost() {
+    // The event parameter holds information about the event that triggered the function. In this case, the event is the change event on the textarea element.
+    function changeBodyHandler(event) {
+        console.log(event.target.value);
+    }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        {/* //React will set up an event listener for the onChange event on the textarea element. Whenever the textarea changes, the changeBodyHandler function will be executed (target for the event listener). */}
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} /> 
       </p>
       <p>
         <label htmlFor="name">Your name</label>
