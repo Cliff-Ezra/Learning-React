92acf86edbef11218cadb0fd603d9656688eb4a6
Cliff-Ezra <cliffezra212@gmail.com>
2023-02-18 10:23:46 +0300

    Rendering Content Conditionally using 3 different Methods

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,10 +2,10 @@ import classes from './Modal.module.css';
 
 //Instead of using pros. you can use object destructuring to get the children prop directly.
 //The NewPost component is passed as a child to the Modal component.
-function Modal({ children }) {
+function Modal({ children, onClose }) {
     return (
     <>
-    <div className={classes.backdrop} />
+    <div className={classes.backdrop} onClick={onClose} />
     <dialog open="true" className={classes.modal}>
         {children}
     </dialog>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -8,22 +8,56 @@ import classes from './PostsList.module.css';
 function PostsList() {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
-    
+    const [modalIsVisible, setModalIsVisible] = useState(true); 
+
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
     }
     function authorChangeHandler(event) {
         setEnteredAuthor(event.target.value);
     }
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    // Method 2
+    let modalContent;
+    if (modalIsVisible) {
+        modalContent = (
+            <Modal onClose={hideModalHandler}>
+                <NewPost 
+                    onBodyChange={bodyChangeHandler} 
+                    onAuthorChange={authorChangeHandler} 
+                />
+            </Modal>
+        );
+    }
 
     return (
         <>
-        <Modal>
-            <NewPost 
-                onBodyChange={bodyChangeHandler} 
-                onAuthorChange={authorChangeHandler} 
-            />
-        </Modal>
+        {/* Method 1 */}
+        {/* {modalIsVisible ? (
+            <Modal onClose={hideModalHandler}>
+                <NewPost 
+                    onBodyChange={bodyChangeHandler} 
+                    onAuthorChange={authorChangeHandler} 
+                />
+            </Modal>
+        ) : false} */}
+
+        {/* Method 2  continuation*/}
+        {/* {modalContent} */}
+
+        {/* Method 3 (truthy & falsy value)*/}
+        {modalIsVisible && (
+            <Modal onClose={hideModalHandler}>
+                <NewPost 
+                    onBodyChange={bodyChangeHandler} 
+                    onAuthorChange={authorChangeHandler} 
+                />
+            </Modal>
+        )}
+
         <ul className={classes.posts}>
             <Post author={enteredAuthor} body={enteredBody} />  
             <Post author="Manuel" body="I'm learning it as well" />
