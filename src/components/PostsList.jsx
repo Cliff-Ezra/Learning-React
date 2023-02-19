c657f729e8d0fe3b5c02bc86c2da200ef6d667b7
Cliff-Ezra <cliffezra212@gmail.com>
2023-02-19 13:50:58 +0300

    Refactored route compoents and added more nesting

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,75 +1,66 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import NewPost from './NewPost';
-import Post from './Post';
-import Modal from './Modal';
-import classes from './PostsList.module.css';
+import classes from "./PostsList.module.css";
+import Post from "./Post";
 
-function PostsList({isPosting, onStopPosting}) {
-    const [posts, setPosts] = useState([]);
+function PostsList() {
+  const [posts, setPosts] = useState([]);
 
-    const [isFetching, setIsFetching] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
 
-    //UseEffect is a hook that allows you to run code when a component is rendered for the first time or when a component is re-rendered
-    // It accepts 2 arguments: a function and an array of dependencies
-    // The function returns a cleanup function that runs before the next time the effect runs (it should not return a promise)
-    // The array of dependencies is optional. If you pass an empty array, the effect will only run once when the component is rendered for the first time
-    useEffect(() => {
-        async function fetchPosts() {
-            setIsFetching(true);
-            const response = await fetch('http://localhost:8085/posts');
-            const resData = await response.json();
-            setPosts(resData.posts);
-            setIsFetching(false);
-        }
-        
-        fetchPosts();
-    }, []);
-
-
-    function addPostHandler(postData) {
-        //  We configure this request because by default fetch sends a get request 
-        fetch('http://localhost:8085/posts', {
-            method: 'POST',
-            body: JSON.stringify(postData), 
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        // Rule: Never mutate state directly 
-        // Explanation: If you update state and the new state is based on the previous state, use the function form of setState
-        setPosts((existingPosts) => [postData, ...existingPosts]);
+  //UseEffect is a hook that allows you to run code when a component is rendered for the first time or when a component is re-rendered
+  // It accepts 2 arguments: a function and an array of dependencies
+  // The function returns a cleanup function that runs before the next time the effect runs (it should not return a promise)
+  // The array of dependencies is optional. If you pass an empty array, the effect will only run once when the component is rendered for the first time
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8085/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
     }
 
-    return (
-        <>
-        {/* Method 3 (truthy & falsy value)*/}
-        {isPosting && (
-            <Modal onClose={onStopPosting}>
-                <NewPost 
-                    onCancel={onStopPosting}
-                    onAddPost={addPostHandler}
-                />
-            </Modal>
-        )}
-        {!isFetching && posts.length > 0 && (
-            <ul className={classes.posts}>
-            {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
-            </ul>
-        )}
-        {!isFetching && posts.length === 0 && (
-            <div style={{ textAlign: 'center', color: 'white' }}>
-                <h2>There are no posts yet.</h2>
-                <p>Be the first to post something!</p>
-            </div>
-        )}
-        {isFetching && (
-            <div style={{ textAlign: 'center', color: 'white' }}>
-                <p>Loading posts...</p>
-            </div>
-            )}
-        </>
-    )
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(postData) {
+    //  We configure this request because by default fetch sends a get request
+    fetch("http://localhost:8085/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // Rule: Never mutate state directly
+    // Explanation: If you update state and the new state is based on the previous state, use the function form of setState
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Be the first to post something!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default PostsList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import './index.css'
-import App from './App'
-import NewPost from './components/NewPost'
-import RootLayout from '../routes/RootLayout'
+import "./index.css";
+import Posts from "./routes/Posts";
+import NewPost from "./routes/NewPost";
+import RootLayout from "./routes/RootLayout";
 
 const router = createBrowserRouter([
-  { path: '/', 
-    element: <RootLayout />, 
+  {
+    path: "/",
+    element: <RootLayout />,
     children: [
-    { path: '/', element: <App /> }, 
-    { path: '/create-post', element: <NewPost /> }
-  ] 
-},
+      { path: "/", 
+        element: <Posts />, 
+        children: [
+          { path: "/create-post", 
+            element: <NewPost /> }
+        ] }
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
