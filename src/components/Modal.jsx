5c52f98cd32c47efe95d3ebe5174b61771dcda1c
Cliff-Ezra <cliffezra212@gmail.com>
2023-02-19 14:22:54 +0300

    Added links and navigation between different routes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 import classes from './Modal.module.css';
 
 //Instead of using pros. you can use object destructuring to get the children prop directly.
 //The NewPost component is passed as a child to the Modal component.
-function Modal({ children, onClose }) {
+function Modal({ children }) {
+    const navigate = useNavigate();
+
+    function closeHandler() {
+        navigate('..');
+    }
+
     return (
     <>
-    <div className={classes.backdrop} onClick={onClose} />
+    <div className={classes.backdrop} onClick={closeHandler} />
     <dialog open className={classes.modal}>
         {children}
     </dialog>
