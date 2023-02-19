577f88757a4c4690a7ba9ef81444166118187377
Cliff-Ezra <cliffezra212@gmail.com>
2023-02-19 10:43:44 +0300

    Added Handling 'loading' state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -8,15 +8,19 @@ import classes from './PostsList.module.css';
 function PostsList({isPosting, onStopPosting}) {
     const [posts, setPosts] = useState([]);
 
+    const [isFetching, setIsFetching] = useState(false);
+
     //UseEffect is a hook that allows you to run code when a component is rendered for the first time or when a component is re-rendered
     // It accepts 2 arguments: a function and an array of dependencies
     // The function returns a cleanup function that runs before the next time the effect runs (it should not return a promise)
     // The array of dependencies is optional. If you pass an empty array, the effect will only run once when the component is rendered for the first time
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8085/posts');
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
         
         fetchPosts();
@@ -48,17 +52,22 @@ function PostsList({isPosting, onStopPosting}) {
                 />
             </Modal>
         )}
-        {posts.length > 0 && (
+        {!isFetching && posts.length > 0 && (
             <ul className={classes.posts}>
             {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
             </ul>
         )}
-        {posts.length === 0 && (
+        {!isFetching && posts.length === 0 && (
             <div style={{ textAlign: 'center', color: 'white' }}>
                 <h2>There are no posts yet.</h2>
                 <p>Be the first to post something!</p>
             </div>
         )}
+        {isFetching && (
+            <div style={{ textAlign: 'center', color: 'white' }}>
+                <p>Loading posts...</p>
+            </div>
+            )}
         </>
     )
 }
